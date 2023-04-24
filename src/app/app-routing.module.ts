5f10ac1973b8ef0127f26cf8f5ba3d2c90944e1e
Jess2315 <munozjb@globalhitss.com>
2023-04-23 22:17:21 -0500

    componet
    componente

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { PantallaExamenComponent } from './pages/pantalla-examen/pantalla-examen.component';
+
+const routes: Routes = [
+
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: 'inicio', component: PantallaExamenComponent }
+];
+
+@NgModule(
+    {
+        declarations: [],
+        imports: [
+            RouterModule.forRoot(routes)
+        ],
+        exports: [RouterModule]
+    }
+)
+export class AppRoutingModule { }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PantallaExamenComponent } from './pages/pantalla-examen/pantalla-examen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantallaExamenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
